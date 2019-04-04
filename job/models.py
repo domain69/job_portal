5940700eb9084bbc94759b2e0c3cb8cc36a7e4ee
Hitesh shetty <hitesh.shetty2011@gmail.com>
2019-04-04 19:30:25 +0530

    converted dyanamic to static for "post a job"

--- job/models.py
+++ job/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    comp_name = models.CharField(max_length=30)
+    comp_username = models.CharField(max_length=20)
+    comp_password = models.CharField(max_length=16)
+    comp_phoneno = models.CharField(max_length=13)
+    comp_email = models.EmailField(max_length=100)
+    comp_description = models.CharField(max_length=300)
+    comp_addressline1 = models.CharField(max_length=100)
+    comp_addressline2 = models.CharField(max_length=100)
+    comp_zipcode = models.CharField(max_length=20)
+    comp_state = models.CharField(max_length=100)
+    comp_country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.comp_name
+
+class Jobfield(models.Model):
+    job_comp = models.ForeignKey(Company,on_delete=models.CASCADE)
+    job_description = models.CharField(max_length=300)
+    job_category = models.CharField(max_length=100)
+    job_experience = models.CharField(max_length=20)
+    job_location = models.CharField(max_length=50)
+    job_salary = models.CharField(max_length=100)
+    job_postedon = models.DateTimeField('date published', auto_now_add=True)
+
+    def __str__(self):
+        return self.job_category
--- job/views.py
+++ job/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import Jobfield,Company
 # Create your views here.
 def index(request):
     return render(request,'job/index.html')
 
 def blog(request):
-    return render(request,'job/blog.html')
+    job=Jobfield.objects.all()
+    return render(request,'job/blog.html',{'jobs':job})
 
 def job_details(request):
     return render(request,'job/blog-details.html')
