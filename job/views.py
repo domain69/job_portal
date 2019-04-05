2902332bdbdbc1d3785d3e3702ffca790367ec3d
Hitesh shetty <hitesh.shetty2011@gmail.com>
2019-04-05 07:25:31 +0530

    signup_comp

--- job/views.py
+++ job/views.py
@@ -13,4 +13,18 @@ def job_details(request,id):
     return render(request,'job/blog-details.html',{'job':job_detail})
 
 def comp_signup(request):
+    if request.method=="POST":
+        name=request.method.get('company')
+        user_id=request.method.get('username')
+        password=request.method.get('first_name')
+        desc=request.method.get('additional')
+        add1=request.method.get('street')
+        add2=request.method.get('street1')
+        state=request.method.get('state')
+        zip1=request.method.get('zip')
+        city=request.method.get('city')
+        phone=request.method.get('phone')
+        email=request.method.get('your_email')
+        comp_obj=Company(comp_name =name,comp_username =user_id,comp_password =password,comp_phoneno =phone,comp_email =email,comp_description =desc,comp_addressline1 =add1,comp_addressline2 =add2,comp_zipcode =zip1,comp_state =state,comp_country =city)
+        comp_obj.save()    
     return render(request,'job/company-signup.html')
